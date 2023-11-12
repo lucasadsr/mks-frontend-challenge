bd0ef7c5e6ab2f0bd48efbb59b7eb8ed092aa6de
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-11-12 19:28:27 -0300

    chore: stitches config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyles()
+
   return <Component {...pageProps} />
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from "@stitches/react";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box'
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: '#F9F9F9',
+    color: '#2C2C2C'
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Montserrat, sans-serif',
+    fontWeight: 400
+  }
+})
